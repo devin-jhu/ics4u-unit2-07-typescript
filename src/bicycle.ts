abee850c42f9f4504b90992973bc592f48842898
devin jhu <devin.jhu@mths.ca>
2022-11-05 04:38:14 +0000

    code

---
+++ src/bicycle.ts
@@ -0,0 +1,42 @@
+/**
+ * the stack calling program
+ *
+ * By:      Devin Jhu
+ * Version: 1.0
+ * Since:   2022-11-4
+ */
+
+import Vehicle from './vehicle'
+
+class Bicycle extends Vehicle {
+  private cadence = 0
+
+  // cadence getter
+  getCadence(): number {
+    return this.cadence
+  }
+
+  // this method calculates the acceleration
+  accelerate(appliedPower: number): void {
+    this.cadence = this.cadence + appliedPower
+    super.setSpeed(this.cadence * 2)
+  }
+
+  // this method displays text
+  ringBell(): void {
+    console.log('ding ding')
+  }
+
+  // this method displays information about the bike
+  status(): void {
+    console.log(`
+    ---> Speed: ${super.getSpeed()}
+    ---> MaxSpeed: ${super.getMaxSpeed()}
+    ---> Color: ${super.getColor()}
+    ---> Cadence: ${this.cadence}
+    `)
+  }
+}
+
+// export class
+export = Bicycle
